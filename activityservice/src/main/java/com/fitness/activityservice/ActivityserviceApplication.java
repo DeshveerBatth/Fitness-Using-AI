89c993bbafa320d1f69999192923ff0252b37cb5
Deshveer Singh Batth <singhdeshveer2004@gmail.com>
2025-06-30 18:31:45 +0530

    Commit

--- activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -2,7 +2,6 @@ package com.fitness.activityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 //@EnableMongoAuditing
