b6700a20057c5db9ce79c95191f75d28d59bebd7
Deshveer Singh Batth <singhdeshveer2004@gmail.com>
2025-07-11 16:42:12 +0530

    Fix: Rename authslice to authSlice with proper casing

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,11 +1,29 @@
+import { Button } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from 'react-oauth2-code-pkce';
+import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { setCredentials } from './store/authSlice';
 
 function App() {
-  
+  const { token, tokenData, logIn, logOut, isAuthenticated } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Button variant="contained" sx={{ backgroundColor: 'black', color: 'white' }}>
+        LOGIN
+      </Button>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- fitness-app-frontend/src/main.jsx
+++ fitness-app-frontend/src/main.jsx
@@ -1,14 +1,21 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './store/store';
+import { store } from './store/store';
 import App from './App';
 
+import { AuthProvider } from 'react-oauth2-code-pkce'; 
+import {authConfig} from './authConfig';     
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <AuthProvider authConfig={authConfig}
+                  loadingComponent = {<div> Loading...</div>}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthProvider>
   </StrictMode>
 );
